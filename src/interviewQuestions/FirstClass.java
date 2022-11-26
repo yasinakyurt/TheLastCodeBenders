ce8bb1a683d3a193270bd3c598fc5f239d2d511a
yasin akyurt <ysnakyurt@gmail.com>
2022-11-26 13:37:26 +0100

    turkce metin girildi.

--- src/interviewQuestions/FirstClass.java
+++ src/interviewQuestions/FirstClass.java
@@ -3,5 +3,6 @@ package interviewQuestions;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World !");
+        System.out.println("Merhaba Dünya");
     }
 }
